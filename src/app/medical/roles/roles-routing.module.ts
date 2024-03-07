8fb491ef5c279e0d6e5bf0d3da861620915cb97a
Rafael Francisco Serrano Urtecho <rafa.tesla@gmail.com>
2024-03-07 09:25:22 -0800

    enrutamiento basico

--- src/app/medical/roles/roles-routing.module.ts
+++ src/app/medical/roles/roles-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RolesComponent } from './roles.component';
+import { AddRoleUserComponent } from './add-role-user/add-role-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:RolesComponent,
+    children:[
+      {
+      path:'register',
+      component:AddRoleUserComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
