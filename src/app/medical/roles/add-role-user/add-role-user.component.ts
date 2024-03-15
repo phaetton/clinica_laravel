c04cf25abc9f06e01e7b95e7378247b1782c37f3
Rafael Francisco Serrano Urtecho <rafa.tesla@gmail.com>
2024-03-15 03:05:59 -0700

    Update add-role-user.component.ts

--- src/app/medical/roles/add-role-user/add-role-user.component.ts
+++ src/app/medical/roles/add-role-user/add-role-user.component.ts
@@ -6,15 +6,31 @@ import { DataService } from 'src/app/shared/data/data.service';
   templateUrl: './add-role-user.component.html',
   styleUrls: ['./add-role-user.component.scss']
 })
-export class AddRoleUserComponent  implements OnInit{
-  sideBar:any=[];
+export class AddRoleUserComponent implements OnInit {
+  sideBar: any = [];
+  name: string = '';
+  permission: any = [];
   constructor(
-    public DataService:DataService
-  ){
+    public DataService: DataService
+  ) {
 
   }
 
-  ngOnInit():void{
-    this.sideBar= this.DataService.sideBar[0].menu;
+  ngOnInit(): void {
+    this.sideBar = this.DataService.sideBar[0].menu;
+  }
+
+
+  addPermission(subMenu: any) {
+    let INDEX = this.permission.findIndex((item: any) => item == subMenu.permision);
+
+    if (INDEX != -1) {
+      this.permission.splice(INDEX, 1);
+    } else {
+        this.permission.push(subMenu.permision);
+    }
+
+    console.log(this.permission);
+    
   }
 }
