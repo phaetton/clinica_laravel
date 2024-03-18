0d278073f79e9fbb4471d3bb34dc1784e3e6cc71
Rafael Francisco Serrano Urtecho <rafa.tesla@gmail.com>
2024-03-18 12:14:58 -0700

    Agregar Rol
    Agregar rol funcionando desde ambos lados

--- src/app/medical/roles/add-role-user/add-role-user.component.ts
+++ src/app/medical/roles/add-role-user/add-role-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/shared/data/data.service';
+import { RolesService } from '../service/roles.service';
 
 @Component({
   selector: 'app-add-role-user',
@@ -10,8 +11,10 @@ export class AddRoleUserComponent implements OnInit {
   sideBar: any = [];
   name: string = '';
   permission: any = [];
+  valid_form: boolean = false;
   constructor(
-    public DataService: DataService
+    public DataService: DataService,
+    public roleService: RolesService
   ) {
 
   }
@@ -27,10 +30,27 @@ export class AddRoleUserComponent implements OnInit {
     if (INDEX != -1) {
       this.permission.splice(INDEX, 1);
     } else {
-        this.permission.push(subMenu.permision);
+      this.permission.push(subMenu.permision);
     }
 
     console.log(this.permission);
+
+  }
+
+  save() {
+    this.valid_form = false; 
+    console.log("Enviando",this.name ,this.permission);
     
+    if (!this.name || !this.permission) {
+      this.valid_form = true;
+      return;
+    }
+    let data = {
+      name: this.name,
+      permisions: this.permission
+    };
+    this.roleService.storeRoles(data).subscribe((resp: any) => {
+      console.log(resp);
+    })
   }
 }
