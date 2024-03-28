fc6b691fb83a4a18fc22555f95b1c3f7db7ee615
Rafael Francisco Serrano Urtecho <rafa.tesla@gmail.com>
2024-03-28 04:44:36 -0700

    endpoint get staff

--- src/app/medical/staff/add-staff-n/add-staff-n.component.ts
+++ src/app/medical/staff/add-staff-n/add-staff-n.component.ts
@@ -6,17 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-staff-n.component.scss']
 })
 export class AddStaffNComponent {
-  public selectedValue !: string  ;
+  public selectedValue !: string;
 
-  public name:string='';
-  public surname:string='';
-  public mobile:string='';
-  public email:string='';
-  public password:string='';
-  public password_confirmation:string='';
-  public birth:string='';
-  public gender:number=1;
-  public education:string='';
-  public designation:string='';
-  public address:string='';
+  public name: string = '';
+  public surname: string = '';
+  public mobile: string = '';
+  public email: string = '';
+  public password: string = '';
+  public password_confirmation: string = '';
+  public birth: string = '';
+  public gender: number = 1;
+  public education: string = '';
+  public designation: string = '';
+  public address: string = '';
+
+  
 }
--- src/app/medical/staff/service/staff.service.ts
+++ src/app/medical/staff/service/staff.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from 'src/app/shared/auth/auth.service';
+import { URL_SERVICIOS } from 'src/config/config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StaffService {
 
-  constructor() { }
+  constructor(
+    public http: HttpClient,
+    public authService: AuthService
+  ) { }
+
+  listUsers() {
+    let headers = new HttpHeaders({ 'Authorization': 'Bearer' + this.authService.token });
+    let URL = URL_SERVICIOS+"staffs";
+    return this.http.get(URL,{headers:headers});
+  }
 }
