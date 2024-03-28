ed99e0ebca6d01c16675b2489ba54dfb221ed6cb
Rafael Francisco Serrano Urtecho <rafa.tesla@gmail.com>
2024-03-28 04:16:40 -0700

    Add staff vista

--- src/app/medical/staff/add-staff-n/add-staff-n.component.ts
+++ src/app/medical/staff/add-staff-n/add-staff-n.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-staff-n.component.scss']
 })
 export class AddStaffNComponent {
+  public selectedValue !: string  ;
 
+  selectedList1: any[] = [
+    {value: 'Select  Department'},
+    {value: 'Orthopedics'},
+    {value: 'Radiology'},
+    {value: 'Dentist'},
+  ];
+  selectedList2: any[] = [
+    {value: 'Select City'},
+    {value: 'Alaska'},
+    {value: 'Los Angeles'},
+  ];
+  selectedList3: any[] = [
+    {value: 'Select Country'},
+    {value: 'Usa'},
+    {value: 'Uk'},
+    {value: 'Italy'},
+  ];
+  selectedList4: any[] = [
+    {value: 'Select State'},
+    {value: 'Alaska'},
+    {value: 'California'},
+  ];
 }
