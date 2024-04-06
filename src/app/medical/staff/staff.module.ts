41742f76059534570f42136500c41cfaff15293a
Rafael Francisco Serrano Urtecho <rafa.tesla@gmail.com>
2024-04-06 11:08:28 -0700

    poder cargar roles en addstaff

--- src/app/medical/staff/staff.module.ts
+++ src/app/medical/staff/staff.module.ts
@@ -9,6 +9,7 @@ import { ListStaffNComponent } from './list-staff-n/list-staff-n.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
@@ -25,7 +26,8 @@ import { RouterModule } from '@angular/router';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RouterModule
+    RouterModule,
+    SharedModule
   ]
 })
 export class StaffModule { }
