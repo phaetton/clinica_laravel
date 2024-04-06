b58dcbc56a055a177d932d5c1fc7d7209244ba7c
Rafael Francisco Serrano Urtecho <rafa.tesla@gmail.com>
2024-04-06 02:49:40 -0700

    servicios staff n

--- src/app/medical/staff/service/staff.service.ts
+++ src/app/medical/staff/service/staff.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AuthService } from 'src/app/shared/auth/auth.service';
+import { URL_SERVICIOS } from 'src/config/config';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StaffService {
+  constructor(
+    public http: HttpClient,
+    public authService: AuthService
+  ) { }
 
-  constructor() { }
+  listUsers() {
+    let headers = new HttpHeaders({ 'Authorization': 'Bearer ' + this.authService.token });
+    let URL = URL_SERVICIOS + "/staffs";
+    return this.http.get(URL, { headers: headers });
+  }
+
+
+  listConfig() {
+    let headers = new HttpHeaders({ 'Authorization': 'Bearer ' + this.authService.token });
+    let URL = URL_SERVICIOS + "/staffs/config";
+    return this.http.get(URL, { headers: headers });
+  }
 }
