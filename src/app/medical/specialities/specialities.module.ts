b523008322263de3f32eef2b24601162a3b905be
Rafael Francisco Serrano Urtecho <rafa.tesla@gmail.com>
2024-05-22 09:50:28 -0700

    Update specialities.module.ts

--- src/app/medical/specialities/specialities.module.ts
+++ src/app/medical/specialities/specialities.module.ts
@@ -9,6 +9,7 @@ import { ListSpecialitieComponent } from './list-specialitie/list-specialitie.co
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
@@ -26,7 +27,8 @@ import { RouterModule } from '@angular/router';
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RouterModule
+    RouterModule,
+    SharedModule
   ]
 })
 export class SpecialitiesModule { }
