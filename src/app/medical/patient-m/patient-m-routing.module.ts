2df49f6ac698cee0124feec421c32aee0b53b10c
Rafael Francisco Serrano Urtecho <rafa.tesla@gmail.com>
2024-05-22 08:03:09 -0700

    Update patient-m-routing.module.ts

--- src/app/medical/patient-m/patient-m-routing.module.ts
+++ src/app/medical/patient-m/patient-m-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddPatientMComponent } from './add-patient-m/add-patient-m.component';
+import { EditPatientMComponent } from './edit-patient-m/edit-patient-m.component';
+import { ListPatientMComponent } from './list-patient-m/list-patient-m.component';
+import { PatientMComponent } from './patient-m.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: PatientMComponent,
+    children: [
+      {
+        path: 'register',
+        component: AddPatientMComponent,
+      },
+      {
+        path: 'list',
+        component: ListPatientMComponent,
+      },
+      {
+        path: 'list/edit/:id',
+        component: EditPatientMComponent,
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
