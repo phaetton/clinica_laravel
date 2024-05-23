972a3143d4bb71fe983a220ddc6c4398a026d2ba
Rafael Francisco Serrano Urtecho <rafa.tesla@gmail.com>
2024-05-23 04:37:17 -0700

    Update roles.service.ts

--- src/app/medical/roles/service/roles.service.ts
+++ src/app/medical/roles/service/roles.service.ts
@@ -12,33 +12,33 @@ export class RolesService {
     public authService: AuthService,
   ) { }
 
-  listRoles() {
-    let headers = new HttpHeaders({ 'Authorization': 'Bearer ' + this.authService.token });
-    let URL = URL_SERVICIOS + "/roles";
-    return this.http.get(URL, { headers: headers });
+  listRoles(){
+    let headers = new HttpHeaders({'Authorization': 'Bearer '+this.authService.token});
+    let URL = URL_SERVICIOS+"/roles";
+    return this.http.get(URL,{headers: headers});
   }
 
-  showRoles(role_id: string) {
-    let headers = new HttpHeaders({ 'Authorization': 'Bearer ' + this.authService.token });
-    let URL = URL_SERVICIOS + "/roles/" + role_id;
-    return this.http.get(URL, { headers: headers });
+  showRoles(role_id:string){
+    let headers = new HttpHeaders({'Authorization': 'Bearer '+this.authService.token});
+    let URL = URL_SERVICIOS+"/roles/"+role_id;
+    return this.http.get(URL,{headers: headers});
   }
 
-  storeRoles(data: any) {
-    let headers = new HttpHeaders({ 'Authorization': 'Bearer ' + this.authService.token });
-    let URL = URL_SERVICIOS + "/roles";
-    return this.http.post(URL, data, { headers: headers });
+  storeRoles(data:any){
+    let headers = new HttpHeaders({'Authorization': 'Bearer '+this.authService.token});
+    let URL = URL_SERVICIOS+"/roles";
+    return this.http.post(URL,data,{headers: headers});
   }
 
-  editRoles(data: any, id_role: any) {
-    let headers = new HttpHeaders({ 'Authorization': 'Bearer ' + this.authService.token });
-    let URL = URL_SERVICIOS + "/roles/" + id_role;
-    return this.http.put(URL, data, { headers: headers });
+  editRoles(data:any,id_role:any){
+    let headers = new HttpHeaders({'Authorization': 'Bearer '+this.authService.token});
+    let URL = URL_SERVICIOS+"/roles/"+id_role;
+    return this.http.put(URL,data,{headers: headers});
   }
 
-  deleteRoles(id_role: any) {
-    let headers = new HttpHeaders({ 'Authorization': 'Bearer ' + this.authService.token });
-    let URL = URL_SERVICIOS + "/roles/" + id_role;
-    return this.http.delete(URL, { headers: headers });
+  deleteRoles(id_role:any){
+    let headers = new HttpHeaders({'Authorization': 'Bearer '+this.authService.token});
+    let URL = URL_SERVICIOS+"/roles/"+id_role;
+    return this.http.delete(URL,{headers: headers});
   }
 }
