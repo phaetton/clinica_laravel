cb83929f21e59e023203964e3075d804512a31ca
Rafael Francisco Serrano Urtecho <rafa.tesla@gmail.com>
2024-05-23 04:34:40 -0700

    corrigiendo y actualizando especialidades

--- src/app/medical/specialitie/specialitie-routing.module.ts
+++ src/app/medical/specialitie/specialitie-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SpecialitieComponent } from './specialitie.component';
+import { AddSpecialitieComponent } from './add-specialitie/add-specialitie.component';
+import { ListSpecialitieComponent } from './list-specialitie/list-specialitie.component';
+import { EditSpecialitieComponent } from './edit-specialitie/edit-specialitie.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: SpecialitieComponent,
+    children: [
+      {
+        path: 'register',
+        component: AddSpecialitieComponent
+      },
+      {
+        path: 'list',
+        component: ListSpecialitieComponent
+      },
+      {
+        path: 'list/edit/:id',
+        component: EditSpecialitieComponent
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
