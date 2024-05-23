40f0bc35cb5f1f043b5c3e04a0ba8f63765eb96f
Rafael Francisco Serrano Urtecho <rafa.tesla@gmail.com>
2024-05-23 04:48:14 -0700

    configurando rutas cliente

--- src/app/medical/patient-m/patient-m-routing.module.ts
+++ src/app/medical/patient-m/patient-m-routing.module.ts
@@ -4,27 +4,30 @@ import { AddPatientMComponent } from './add-patient-m/add-patient-m.component';
 import { EditPatientMComponent } from './edit-patient-m/edit-patient-m.component';
 import { ListPatientMComponent } from './list-patient-m/list-patient-m.component';
 import { PatientMComponent } from './patient-m.component';
+import { PatientMProfileComponent } from './patient-m-profile/patient-m-profile.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: PatientMComponent,
-    children: [
-      {
-        path: 'register',
-        component: AddPatientMComponent,
-      },
-      {
-        path: 'list',
-        component: ListPatientMComponent,
-      },
-      {
-        path: 'list/edit/:id',
-        component: EditPatientMComponent,
-      }
-    ]
-  }
-];
+const routes: Routes = [{
+  path: '',
+  component: PatientMComponent,
+  children: [
+    {
+      path: 'register',
+      component: AddPatientMComponent,
+    },
+    {
+      path: 'list',
+      component: ListPatientMComponent,
+    },
+    {
+      path: 'list/edit/:id',
+      component: EditPatientMComponent,
+    },
+    {
+      path: 'list/profile/:id',
+      component: PatientMProfileComponent,
+    }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
